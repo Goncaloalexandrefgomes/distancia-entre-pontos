b87fd8092382edb23bca5c4364ae7284e9b3b563
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-12-02 16:47:42 +0000

    fimm

--- distancia entre pontos/Form1.cs
+++ distancia entre pontos/Form1.cs
@@ -17,15 +17,16 @@ namespace distancia_entre_pontos
             InitializeComponent();
         }
 
-        dist distancia = new dist();
+        dist P1 = new dist();
+        dist P2 = new dist();
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            distancia.PontoX1 = X1.Text;
-            distancia.PontoX2 = X2.Text;
-            distancia.PontoY1 = Y1.Text;
-            distancia.PontoY2 = Y2.Text;
-            label4.Text = distancia.Pontos;
+            P1.X = Convert.ToDouble(X1.Text);
+            P1.Y = Convert.ToDouble(Y1.Text);
+            P2.X = Convert.ToDouble(X1.Text);
+            P2.Y = Convert.ToDouble(Y2.Text);
+            label4.Text = (Math.Sqrt(Math.Pow(P2.X-P1.X,2)+ Math.Pow(P2.Y - P1.Y, 2))).ToString();
         }
     }
 }
--- distancia entre pontos/dist.cs
+++ distancia entre pontos/dist.cs
@@ -8,15 +8,9 @@ namespace distancia_entre_pontos
 {
     class dist
     {
-        private double _x1, _x2, _y1, _y2;
+        private double _X, _Y;
 
-        public string PontoX1 { set => _x1 = Convert.ToDouble(value); }
-        public string PontoX2 { set => _x2 = Convert.ToDouble(value); }
-        public string PontoY1 { set => _y1 = Convert.ToDouble(value); }
-        public string PontoY2 { set => _y2 = Convert.ToDouble(value); }
-
-
-        public string Pontos => (Math.Sqrt((_x2 - _x1) * (_x2 - _x1) + (_y2 - _y1) * (_y2 - _y1))).ToString();
-
+        public double X { set => _X = value; get => _X; }
+        public double Y { set => _Y = value; get => _Y; }
     }
 }
